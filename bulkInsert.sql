d7937a28f8af03ab2a8f26db96f3e1ab654344e3
Vivian Nguyen <vyho0711@gmail.com>
2019-05-26 20:37:02 -0400

    Create bulkInsert.sql

---
+++ bulkInsert.sql
@@ -0,0 +1,31 @@
+/*bulk insert using text files*/
+
+
+bulk insert MusicDatabase.dbo.customers
+
+from 'C:\Users\vyho0\Downloads\customer.txt'
+
+with 
+
+(
+
+
+rowterminator = '\n'
+
+)
+
+
+/*insert with delimiter*/
+
+
+bulk insert MusicDatabase.dbo.customers
+
+from 'C:\Users\vyho0\Downloads\customer.txt'
+
+with 
+
+(
+
+fieldterminator = ',', /*delimiter goes here*/
+
+rowterminator = '\n'
